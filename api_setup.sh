0d6b4a010d498fa831720797b901576b38817ce5
Billz <58287516+billz94@users.noreply.github.com>
2022-04-01 22:24:36 +0200

    Create api_setup.sh

---
+++ api_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash --login
+echo "--------------------------------------------"
+echo "Checkout to latest tag"
+echo "--------------------------------------------"
+git checkout v4.15.0-beta -f
+echo "--------------------------------------------"
+echo "Describing Head"
+echo "--------------------------------------------"
+git describe > HEAD
+echo "____________________________________________"
+echo "Removing Gemfile.lock"
+echo "____________________________________________"
+rm Gemfile.lock
+echo "____________________________________________"
+echo "Installing Local Gems"
+echo "____________________________________________"
+bundle install --local
+echo "--------------------------------------------"
+#echo "killing sync_worker cron job"
+#pkill -f bin/lab/sync_worker.rb
+echo "____________________________________________"
+echo "running bin_update art"
+echo "____________________________________________"
+bin/update_art_metadata.sh development
+echo "--------------------------------------------"
+#echo "replace port 5000 in application.yml to 8050"
+#sed -i 's/5000/8050/g' config/application.yml
