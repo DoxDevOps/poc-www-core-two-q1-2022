300c4960d58ade6451c8e4ce217b646eaf1f02e3
Billz <58287516+billz94@users.noreply.github.com>
2022-04-01 14:29:27 +0200

    Update art_shippingx.py

--- art_shippingx.py
+++ art_shippingx.py
@@ -58,9 +58,9 @@ for site_id in cluster['site']:
 
             version = msg.format(result).strip()
 
-            api_version = "v4.14.0"
+            art_version = "v4.14.3-beta"
 
-            if api_version == version:
+            if art_version == version:
                 msgx = "Hi there,\n\nDeployment of ART to " + version + " for " + site[
                     'name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
             else:
@@ -84,7 +84,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of ART to v4.14.0 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
+                    msg = "Hi there,\n\nDeployment of ART to " + version + " for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
