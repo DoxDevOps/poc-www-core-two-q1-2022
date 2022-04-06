3c3d9eab0ca39ff61f08e1bc0a17203ceef15aac
Billz <58287516+billz94@users.noreply.github.com>
2022-04-06 17:49:46 +0200

    Update and rename core_art_setup.sh to art_setup.sh

---
+++ art_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash --login
+echo "____________________________________________"
+echo "Checkout to latest tag in ART"
+echo "____________________________________________"
+git checkout v4.14.3-beta -f
+echo "____________________________________________"
+echo "Describing Head"
+echo "____________________________________________"
+git describe > HEAD
+echo "____________________________________________"
+echo "Making changes to application.json"
+echo "____________________________________________"
+cp application.json.example application.json
+echo "____________________________________________"
