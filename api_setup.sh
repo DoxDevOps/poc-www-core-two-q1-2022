e62da2474af6c3a1ed6281822ac0bca5fa741098
Billz <58287516+billz94@users.noreply.github.com>
2022-04-07 13:33:12 +0200

    Update api_setup.sh

--- api_setup.sh
+++ api_setup.sh
@@ -12,16 +12,16 @@ echo "Removing Gemfile.lock"
 #echo "____________________________________________"
 #rm Gemfile.lock
 #echo "____________________________________________"
-#echo "Installing Local Gems"
-#echo "____________________________________________"
-#bundle install --local
+echo "Installing Local Gems"
+echo "____________________________________________"
+bundle install --local
 echo "--------------------------------------------"
 #echo "killing sync_worker cron job"
 #pkill -f bin/lab/sync_worker.rb
 echo "____________________________________________"
 echo "running bin_update art"
 echo "____________________________________________"
-bin/update_art_metadata.sh development
+./bin/update_art_metadata.sh development
 echo "--------------------------------------------"
 #echo "replace port 5000 in application.yml to 8050"
 #sed -i 's/5000/8050/g' config/application.yml
